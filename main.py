8c7bd91af04e6f3f01b4c4cf87a240d7f119948c
Melvin Ziemann <ziemann.melvin@gmail.com>
2021-11-09 17:39:08 +0100

    created tformat class

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from classes.directory import Directory
+from classes.tformat import TFormat
 if __name__ == "__main__":
-	dir = Directory()
-	dir.createDir("test-repo", "/home/humanity/")
-	print("end")
+	# class instantiation
+	t = TFormat()
+	dir = Directory("test-repo", "/home/humanity/")
+	# create new directory
+	dir.createDir()
+	del dir
+	
+	# logs end of execution
+	print(t.OKGREEN + t.BOLD + "[end of main]" + t.ENDC)
